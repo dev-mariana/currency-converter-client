701f6714119fc7ebd2c86543992bf37b036260ad
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-02-02 21:22:52 -0300

    project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../../environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'currency-converter-client';
+
+  httpClient = inject(HttpClient);
+
+  currencies: any[] = [];
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.httpClient
+      .get(`${environment.base_url}/currencies`)
+      .subscribe((data: any) => {
+        console.log(data);
+        this.currencies = data;
+      });
+  }
 }
