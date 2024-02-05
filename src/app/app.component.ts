43098986c0b7a19a1c516ebf9bd674d7daed980e
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-02-05 13:23:14 -0300

    refactor: replace environment path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../../environment';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  base_url: 'https://currency-converter-api-production-0f30.up.railway.app',
+};
